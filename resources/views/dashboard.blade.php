aae20f26b7dceaf364a2c7059232da33c825e464
zzzarkaztik <arvsvillalobos@gmail>
2024-02-06 14:33:25 +0800

    added spacing for about

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,27 +17,27 @@
                 <div class="card border">
                     <div class="card-body">
                         <h5 class="card-title p-2 text-white text-center rounded bg-success"><i
-                                class="ri-road-map-fill me-1"></i>Routes</h5>
+                                class="ri-road-map-fill"></i> Routes</h5>
                         <span class="card-text">
                             Total Routes:
                         </span>
                         <h4 class="text-success">$r->total_routes</h4>
 
-                        <a href="/admin/routes" class="btn btn-success">View</a>
+                        <a href="/admin/routes" class="btn btn-success"><i class="ri-eye-fill"></i> View</a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-3">
                 <div class="card border">
                     <div class="card-body">
-                        <h5 class="card-title p-2 text-white text-center rounded"><i class="ri-bus-fill me-1"></i>Buses
+                        <h5 class="card-title p-2 text-white text-center rounded"><i class="ri-bus-fill"></i> Buses
                         </h5>
                         <span class="card-text">
                             Total Routes:
                         </span>
                         <h4>$b->total_buses</h4>
 
-                        <a href="/admin/routes" class="btn btn-primary">View</a>
+                        <a href="/admin/routes" class="btn btn-primary"><i class="ri-eye-fill"></i> View</a>
                     </div>
                 </div>
             </div>
@@ -45,13 +45,13 @@
                 <div class="card border">
                     <div class="card-body">
                         <h5 class="card-title p-2 text-white text-center rounded bg-warning"><i
-                                class="ri-calendar-schedule-fill me-1"></i>Schedules</h5>
+                                class="ri-calendar-schedule-fill"></i> Schedules</h5>
                         <span class="card-text">
                             Total Schedules:
                         </span>
                         <h4 class="text-warning">$s->total_schedules</h4>
 
-                        <a href="/admin/routes" class="btn btn-warning">View</a>
+                        <a href="/admin/routes" class="btn btn-warning"><i class="ri-eye-fill"></i> View</a>
                     </div>
                 </div>
             </div>
@@ -59,12 +59,12 @@
                 <div class="card border">
                     <div class="card-body">
                         <h5 class="card-title p-2 text-white text-center rounded bg-danger"><i
-                                class="ri-coupon-3-fill me-1"></i>Bookings</h5>
+                                class="ri-coupon-3-fill"></i> Bookings</h5>
                         <span class="card-text">
                             Total Bookings:
                         </span>
                         <h4 class="text-danger">$b->total_bookings</h4>
-                        <a href="/admin/routes" class="btn btn-danger">View</a>
+                        <a href="/admin/routes" class="btn btn-danger"><i class="ri-eye-fill"></i> View</a>
                     </div>
                 </div>
             </div>
--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -1,6 +1,5 @@
 <div id="sidebar">
     <a href="/admin/dashboard" class="active">Dashboard</a>
-    <a href="#">POS</a>
     <a href="#">Buses</a>
     <a href="#">Routes</a>
     <a href="#">Bookings</a>
