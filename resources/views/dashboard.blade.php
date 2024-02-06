9492418e904953abed77ea8d9ce2357534246cf8
zzzarkaztik <arvsvillalobos@gmail>
2024-02-06 09:27:52 +0800

    rebase

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('layouts/head')
+    <title>Trident Buslines | Home</title>
+</head>
+
+<body class="mt-5 pt-2">
+    @include('layouts/navbar')
+
+
+    <div class="vh-100 col-lg-3 border">
+        <h1>buses</h1>
+        <h1>buses</h1>
+        <h1>buses</h1>
+        <h1>buses</h1>
+
+    </div>
+
+
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.umd.min.js"></script>
+</body>
+
+</html>
---
+++ resources/views/layouts/navbar_admin.blade.php
@@ -0,0 +1,58 @@
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg bg-body-danger fixed-top">
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+        <!-- Navbar brand -->
+        <a class="navbar-brand mt-2 mt-lg-0" href="#">
+            <img src="img/trident-logo.svg" width="130px" alt="trident logo" loading="lazy" />
+        </a>
+        <!-- Left links -->
+        <div class="d-flex me-auto mb-lg-0">
+            <a class="nav-link me-2" href="/admin/dashboard">DASHBOARD</a>
+            <a class="nav-link me-2" href="/admin/schedules">SCHEDULES</a>
+            <a class="nav-link " href="/admin/book">BOOK A TICKET</a>
+        </div>
+        <!-- Left links -->
+
+        <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1"
+            id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <p>Try scrolling the rest of the page to see this option in action.</p>
+            </div>
+        </div>
+
+        <!-- Right elements -->
+        <div class="d-flex align-items-center">
+            <!-- Avatar -->
+            <div class="dropdown">
+                <a data-mdb-dropdown-init class="dropdown-toggle d-flex align-items-center hidden-arrow" href="#"
+                    id="navbarDropdownMenuAvatar" role="button" aria-expanded="false">
+                    <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+                        class="border border-solid rounded-circle" height="40"
+                        alt="Black and White Portrait of a Man" loading="lazy" />
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuAvatar">
+                    <li>
+                        <a class="dropdown-item" href="#">My profile</a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">Settings</a>
+                    </li>
+                    <li>
+                        <hr class="dropdown-divider" />
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="#">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- Right elements -->
+    </div>
+    <!-- Container wrapper -->
+</nav>
+<!-- Navbar -->
