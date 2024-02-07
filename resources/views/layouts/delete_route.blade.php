db08c8380beb13be6d2c89aa501d5a7bc1e88644
Keith Aquino <aquinokeithvargas@gmail.com>
2024-02-07 15:36:44 +0800

    moved delete route function to the layouts folder

---
+++ resources/views/layouts/delete_route.blade.php
@@ -0,0 +1,22 @@
+<div class="modal fade" id="delete_{{$br -> bus_route_id}}" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Delete route ({{$br -> bus_route_id}}) {{$br -> origin}} to {{$br -> destination}}?</h5>
+                <button class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <div class="modal-body">Once this action is taken, it cannot be undone.
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" data-bs-dismiss="modal">
+                        Cancel
+                    </button>
+                    <form action="/admin/routes/{{$br -> bus_route_id}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <input class="btn btn-danger" type="submit" value="Delete" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/routes.blade.php
+++ resources/views/routes.blade.php
@@ -34,29 +34,7 @@
                     <a href="#" class="btn btn-warning">Edit</a>
                     <a data-bs-toggle="modal" data-bs-target="#delete_{{$br -> bus_route_id}}" class="btn btn-danger">Delete</a>
                 </td>
-
-                <div class="modal fade" id="delete_{{$br -> bus_route_id}}" tabindex="-1">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title">Delete route ({{$br -> bus_route_id}}) {{$br -> origin}} to {{$br -> destination}}?</h5>
-                                <button class="btn-close" data-bs-dismiss="modal"></button>
-                            </div>
-                            <div class="modal-body">Once this action is taken, it cannot be undone.
-                                <div class="modal-footer">
-                                    <button class="btn btn-secondary" data-bs-dismiss="modal">
-                                        Cancel
-                                    </button>
-                                    <form action="/admin/routes/{{$br -> bus_route_id}}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <input class="btn btn-danger" type="submit" value="Delete" />
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @include('layouts/delete_route')
             </tr>
             @endforeach
         </table>
