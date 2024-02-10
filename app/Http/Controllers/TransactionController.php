55697bb4839db9e64dd8298916f03b3d8984ac21
Keith Aquino <aquinokeithvargas@gmail.com>
2024-02-10 20:28:34 +0800

    added some process in transaction(incomplete)

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\BusSchedule;
 use App\Models\Transaction;
 use App\Models\Ticket;
+use App\Models\User;
 
 use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
@@ -19,12 +20,27 @@ class TransactionController extends Controller
 
         $sched = BusSchedule::query()
             ->select('*')
+            ->join('buses as b', 'b.bus_id', '=', 'bus_schedules.bus_id')
+            ->join('bus_routes as br', 'br.bus_route_id', '=', 'b.bus_route_id')
             ->where('available_seats', '>', '0')
             ->get();
 
+        $tickets = [];
         for ($i = 0; $i < count($sched); $i++) {
             $num_book = $r->input('book_' . $sched[$i]->bus_schedule_id);
+            if ($num_book > 0) {
+                $ticket = new Ticket;
+                $ticket->transaction_id = $book->transaction_id;
+                $ticket->bus_schedule_id = $sched[$i]->bus_schedule_id;
+                $user = User::find(Session::get('user_id'));
+                $ticket->type = $user->type;;
+                $ticket->price = $sched[$i]->price;
+                $ticket->save();
+                array_push($tickets, $ticket);
+            }
         }
+
+        return view('tickets', compact('book', 'tickets'));
     }
 
     public function index()
