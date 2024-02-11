ad44fd9c016a0e02edfa5e412c192ac7cfcd49ac
zzzarkaztik <arvsvillalobos@gmail>
2024-02-12 02:10:19 +0800

    for deployment

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -13,14 +13,4 @@ echo "Running migrations..."
 php artisan migrate --force
 
 echo "Running seeders"
-php artisan db:seed --class=ClassesTableSeeder --force
-php artisan db:seed --class=FacultiesEducTableSeeder --force
-php artisan db:seed --class=FacultiesTableSeeder --force
-php artisan db:seed --class=OrdersProductsTableSeeder --force
-php artisan db:seed --class=OrdersTableSeeder --force
-php artisan db:seed --class=ProductsTableSeeder --force
-php artisan db:seed --class=StudentsClassesTableSeeder --force
-php artisan db:seed --class=StudentsPhotosTableSeeder --force
-php artisan db:seed --class=StudentsTableSeeder --force
-php artisan db:seed --class=SubjectsTableSeeder --force
-php artisan db:seed --class=UsersTableSeeder --force
+php artisan db:seed --force
