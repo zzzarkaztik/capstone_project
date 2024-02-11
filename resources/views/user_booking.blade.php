d41429ce0e817c3c8768df655af2db51c2c591a6
pinaypie321 <johnrafaeluchi@gmail.com>
2024-02-12 01:29:39 +0800

    added desgin at user_booking

--- resources/views/user_booking.blade.php
+++ resources/views/user_booking.blade.php
@@ -24,11 +24,36 @@
         });
     </script>
     <style>
-        .btn {
-            border-radius: 50px;
-            size: 20px;
+        .card {
+            border-radius: 15px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            background-color: #d8f9ff;
+            /* Light gray */
+            margin-top: 20px;
+            /* Add margin between cards */
+        }
+
+        .card-title {
+            font-weight: bold;
+            margin-bottom: 10px;
+            background-color: transparent !important;
+        }
+
+        .card-body {
+            padding: 1rem;
+        }
+
+        /* Target only buttons inside cards */
+        .card .btn {
+            border-radius: 15px;
+            /* Rounded corners */
+            padding: 3px 8px;
+            /* Adjust padding as needed */
+            font-size: 0.7rem;
+            /* Adjust font size */
         }
     </style>
+
 </head>
 
 <body>
@@ -40,22 +65,24 @@
                 @csrf
                 @foreach ($sched as $s)
                 <div class="col-lg-3">
-                    <div class="card mt-4 pt-5">
+                    <div class="card">
                         <div class="card-body">
-                            <p>Trip #{{$s -> bus_schedule_id}}</p>
-                            <p>Bus #{{$s -> bus_id}} - {{$s -> destination}}</p>
-                            <p>Bus Arrival: {{$s -> arrival_time}}</p>
-                            <p>Departure Time: {{$s -> departure_time}}</p>
-                            <p>Seats available: {{$s -> available_seats}}</p>
-                            <a class="btn btn-danger deduct_button" id="deduct_{{$s -> bus_schedule_id}}">-</a>
-                            <input type="number" style="width: 50px" min="0" max="1" value="0" id="book_{{$s -> bus_schedule_id}}" name="book_{{$s -> bus_schedule_id}}">
-                            <a class="btn btn-primary add_button" id="add_{{$s -> bus_schedule_id}}">+</a>
+                            <h5 class="card-title">Trip #{{$s->bus_schedule_id}}</h5>
+                            <p>Bus #{{$s->bus_id}} - {{$s->destination}}</p>
+                            <p>Bus Arrival: {{$s->arrival_time}}</p>
+                            <p>Departure Time: {{$s->departure_time}}</p>
+                            <p>Seats available: {{$s->available_seats}}</p>
+                            <a class="btn btn-danger deduct_button" id="deduct_{{$s->bus_schedule_id}}">-</a>
+                            <input type="number" style="width: 50px; height: 25px" min="0" max="1" value="0" id="book_{{$s->bus_schedule_id}}" name="book_{{$s->bus_schedule_id}}">
+                            <a class="btn btn-primary add_button" id="add_{{$s->bus_schedule_id}}">+</a>
                         </div>
                     </div>
                 </div>
                 @endforeach
             </div>
-            <input type="submit" class="btn btn-success mt-3" value="Book Ticket" />
+            <div class="mb-4 pt-4 text-center"><input type="submit" class="btn btn-primary
+             mt-3" value="Book Ticket" /></div>
+
         </form>
     </div>
 
