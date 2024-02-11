0b3b789e673255608bd414faaeb1d9a2edae455e
pinaypie321 <johnrafaeluchi@gmail.com>
2024-02-11 20:15:32 +0800

    reverted changes in transaction controller

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -6,50 +6,41 @@ use App\Models\BusSchedule;
 use App\Models\Transaction;
 use App\Models\Ticket;
 use App\Models\User;
-use Illuminate\Support\Facades\DB;
+
 use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
-    public function show_transactions(Request $r)
-    {
-        $transaction = Transaction::query()
-            ->select('*')
-            ->where('user_id', '=', Session::get('user_id'))
-            ->get();
-        return view('transactions', compact('transaction'));
-    }
     public function book_ticket(Request $r)
     {
-        $book_status = 'pending';
-
         $book = new Transaction;
         $book->user_id = Session::get('user_id');
-        $book->order_status = $book_status;
         $book->save();
 
         $sched = BusSchedule::query()
+            ->select('*')
+            ->join('buses as b', 'b.bus_id', '=', 'bus_schedules.bus_id')
+            ->join('bus_routes as br', 'br.bus_route_id', '=', 'b.bus_route_id')
             ->where('available_seats', '>', '0')
             ->get();
 
         $tickets = [];
         $totalPrice = 0;
-        foreach ($sched as $schedule) {
-            $num_book = $r->input('book_' . $schedule->bus_schedule_id);
+        for ($i = 0; $i < count($sched); $i++) {
+            $num_book = $r->input('book_' . $sched[$i]->bus_schedule_id);
             if ($num_book > 0) {
                 $ticket = new Ticket;
                 $ticket->transaction_id = $book->transaction_id;
-                $ticket->bus_schedule_id = $schedule->bus_schedule_id;
+                $ticket->bus_schedule_id = $sched[$i]->bus_schedule_id;
                 $user = User::find(Session::get('user_id'));
-                $ticket->type = $user->type;
-                $ticket->price = $schedule->price;
+                $ticket->type = $user->type;;
+                $ticket->price = $sched[$i]->price;
                 $ticket->save();
                 $totalPrice += $ticket->price * $num_book;
                 array_push($tickets, $ticket);
             }
         }
-
         $book->total_price = $totalPrice;
         $book->save();
 
@@ -68,13 +59,10 @@ class TransactionController extends Controller
 
     public function index()
     {
-        $schedules = BusSchedule::query()
-            ->select('bus_schedules.*', 'bus_routes.destination')
-            ->join('buses', 'bus_schedules.bus_id', '=', 'buses.bus_id')
-            ->join('bus_routes', 'buses.bus_route_id', '=', 'bus_routes.bus_route_id')
-            ->where('bus_schedules.available_seats', '>', 0)
+        $sched = BusSchedule::query()
+            ->select('*')
+            ->where('available_seats', '>', '0')
             ->get();
-
-        return view('user_booking', compact('schedules'));
+        return view('user_booking', compact('sched'));
     }
 }
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,70 +9,72 @@
 <body>
     @include('layouts/navbar')
     @include('layouts/messages')
-    <div class="vh-100 d-flex justify-content-center align-items-center text-center">
 
-        <div class="m-5 p-5 w-50">
-            <img src="img/trident-logo.png" alt="trident logo" width="250px">
-            <form action="/register" method="POST">
-                @csrf
-                <!-- First Name input -->
-                <div class="form-outline my-4" data-mdb-input-init>
-                    <input type="text" class="form-control" name="first_name" />
-                    <label class="form-label">First Name</label>
-                </div>
+    <div class="container my-5 py-3">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <img src="img/trident-logo.png" alt="trident logo" class="mx-auto mb-4" style="max-width: 250px;">
+                        <form action="/register" method="POST">
+                            @csrf
+                            <!-- First Name input -->
+                            <div class="form-outline mb-4">
+                                <input type="text" class="form-control" name="first_name" />
+                                <label class="form-label">First Name</label>
+                            </div>
 
-                <!-- Last Name input -->
-                <div class="form-outline my-4" data-mdb-input-init>
-                    <input type="text" class="form-control" name="last_name" />
-                    <label class="form-label">Last Name</label>
-                </div>
+                            <!-- Last Name input -->
+                            <div class="form-outline mb-4">
+                                <input type="text" class="form-control" name="last_name" />
+                                <label class="form-label">Last Name</label>
+                            </div>
 
-                <!-- Email input -->
-                <div class="form-outline my-4" data-mdb-input-init>
-                    <input type="email" class="form-control" name="email" />
-                    <label class="form-label">Email address</label>
-                </div>
+                            <!-- Email input -->
+                            <div class="form-outline mb-4">
+                                <input type="email" class="form-control" name="email" />
+                                <label class="form-label">Email address</label>
+                            </div>
 
-                <!-- Password input -->
-                <div class="form-outline mb-4" data-mdb-input-init>
-                    <input type="password" class="form-control" name="pw" />
-                    <label class="form-label">Password</label>
-                </div>
+                            <!-- Password input -->
+                            <div class="form-outline mb-4">
+                                <input type="password" class="form-control" name="pw" />
+                                <label class="form-label">Password</label>
+                            </div>
 
-                <div class="form-outline mb-4" data-mdb-input-init>
-                    <input type="password" class="form-control" name="con_pw" />
-                    <label class="form-label">Confirm Password</label>
-                </div>
+                            <div class="form-outline mb-4">
+                                <input type="password" class="form-control" name="con_pw" />
+                                <label class="form-label">Confirm Password</label>
+                            </div>
 
-                <!-- Passenger Type dropdown -->
-                <div class="form-outline mb-4" data-mdb-input-init>
-                    <select class="form-select" name="type">
-                        <option value="regular">Regular</option>
-                        <option value="senior_citizen">Senior Citizen</option>
-                        <option value="pwd">PWD</option>
-                        <option value="student">Student</option>
-                    </select>
-                    <label class="form-label">Passenger Type</label>
-                </div>
-                {{-- terms and conditions --}}
-                <div class="form-check mb-4">
-                    <input type="checkbox" value="" id="terms_checkbox" />
-                    <label for="terms_checkbox">I accept the <a href="/terms_and_conditions">Terms and
-                            Conditions</a></label>
-                </div>
-                <!-- Submit button -->
-                <button type="submit" class="btn btn-primary btn-block mb-4">Register</button>
+                            <!-- Passenger Type dropdown -->
+                            <div class="form-outline mb-4">
+                                <select class="form-select" name="type">
+                                    <option value="regular">Regular</option>
+                                    <option value="senior_citizen">Senior Citizen</option>
+                                    <option value="pwd">PWD</option>
+                                    <option value="student">Student</option>
+                                </select>
+                                <label class="form-label">Passenger Type</label>
+                            </div>
 
-                <!-- Login link -->
-                <div class="text-center">
-                    <p>Already a member? <a href="/login">Login</a></p>
+                            {{-- terms and conditions --}}
+                            <div class="form-check mb-4">
+                                <input type="checkbox" value="" id="terms_checkbox" class="form-check-input" />
+                                <label for="terms_checkbox" class="form-check-label">I accept the <a href="/terms_and_conditions">Terms and
+                                        Conditions</a></label>
+                            </div>
+                            <!-- Submit button -->
+                            <button type="submit" class="btn btn-primary btn-block mb-4">Register</button>
+
+                            <!-- Login link -->
+                            <p>Already a member? <a href="/login">Login</a></p>
+                        </form>
+                    </div>
                 </div>
-            </form>
+            </div>
         </div>
-
     </div>
-
-
 
 </body>
 
